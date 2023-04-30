b01f4a28251e422ed22609b74f726a4bd6e415e6
Dmytro Iasynskyi <dmytro.iasynskyi@vitechteam.com>
2023-04-30 12:45:27 +0300

    header-implementation

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,9 +1,10 @@
 import "./Header.scss";
-import starIcon from "../icons/star.png";
-import bellIcon from "../icons/bell.png";
-import questionIcon from "../icons/question.png";
-import globeIcon from "../icons/globe.png";
-import userIcon from "../icons/user.png";
+import StarIcon from '@mui/icons-material/Star';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import LanguageIcon from '@mui/icons-material/Language';
+import PersonIcon from '@mui/icons-material/Person';
+import React from "react";
 export default function Header() {
 
     return(
@@ -17,12 +18,12 @@ export default function Header() {
                 <a href="#">Search</a>
             </nav>
             <ul className={"header-social"}>
-                <li><a href="#"><img className={"header-icon"} src={starIcon} alt=""/></a></li>
-                <li><a href="#"><img className={"header-icon"} src={bellIcon} alt=""/></a></li>
-                <li><a href="#"><img className={"header-icon"} src={questionIcon} alt=""/></a></li>
-                <li><a href="#"><img className={"header-icon"} src={globeIcon} alt=""/></a></li>
+                <li><a href="#"><StarIcon className={"header-social-icon"} /></a></li>
+                <li><a href="#"><NotificationsActiveIcon className={"header-social-icon"} /></a></li>
+                <li><a href="#"><QuestionMarkIcon className={"header-social-icon"} /></a></li>
+                <li><a href="#"><LanguageIcon className={"header-social-icon"} /></a></li>
                 <li><a href="#">User User</a></li>
-                <li><a href="#"><img className={"header-icon"} src={userIcon} alt=""/></a></li>
+                <li><a href="#"><PersonIcon className={"header-social-icon"} /></a></li>
             </ul>
         </div>
     )
